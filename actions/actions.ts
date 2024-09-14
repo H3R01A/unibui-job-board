2e6e999055f4f298ee2d070fe239654f163468b1
H3R01A <aallen0001@gmail.com>
2024-09-14 10:28:16 -0700

    update actions.ts with typing

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,33 +1,34 @@
-
 'use server';
 import 'server-only';
-// import xss from 'xss';
-import { redirect } from 'next/navigation';
-//import { revalidatePath } from 'next/cache';
 import Papa from 'papaparse';
 import { promises as fs } from 'fs';
-import path from 'path'
-import {Job} from '../utils/types'
+import path from 'path';
+import { Job } from '../utils/types';
 
+// import xss from 'xss';
+//import { redirect } from 'next/navigation';
+//import { revalidatePath } from 'next/cache';
 
-export async function getJobData(){
-  const csvfilePath = path.join(process.cwd(), 'public', '/data/jobs.csv'); 
-  const csvFile = await fs.readFile(csvfilePath, 'utf8');
-  const parsedData = Papa.parse<Job>(csvFile , { header: true });
-  return parsedData.data
-
+export async function getJobData(): Promise<Job[] | undefined> {
+  try {
+    const csvfilePath = path.join(process.cwd(), 'public', '/data/jobs.csv');
+    const csvFile = await fs.readFile(csvfilePath, 'utf8');
+    const parsedData = Papa.parse<Job>(csvFile, { header: true });
+    return parsedData.data;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(
+        `Something went wrong with trying to fetch data. Look into the following error message: ${error.message}`
+      );
+    } else {
+      console.error('An unknown error occur');
+    }
+  }
 }
 
 export async function getJobDetails(id: number) {
-  const csvfilePath = path.join(process.cwd(), 'public', '/data/jobs.csv'); 
+  const csvfilePath = path.join(process.cwd(), 'public', '/data/jobs.csv');
   const csvFile = await fs.readFile(csvfilePath, 'utf8');
-  const parsedData = Papa.parse<Job>(csvFile , { header: true });
-  return parsedData.data[id]
+  const parsedData = Papa.parse<Job>(csvFile, { header: true });
+  return parsedData.data[id];
 }
-
-export async function handleOrderSubmission(formData: FormData): Promise<void> {
-  const userOrder = formData.get('order');
-  redirect(`/order/${userOrder}`);
-}
-
-
