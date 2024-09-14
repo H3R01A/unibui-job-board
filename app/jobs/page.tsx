bf0ca41b0af21c1644bb549c6fc28436f124889f
H3R01A <aallen0001@gmail.com>
2024-09-14 10:30:34 -0700

    update jobs page.tsx with typing and error handling

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,24 +1,28 @@
 import '../../app/globals.css';
 import styles from './page.module.css';
 import { getJobData } from '@/actions/actions';
+import { Job } from '../../utils/types';
 import JobDetailsButton from '@/components/job-details-button';
 
-export default async function JobListingPage() {
-  const jobData = await getJobData();
+export default async function JobListingsPage() {
+  
+  const jobData: Job[] | undefined = await getJobData();
+
+  if (!jobData) {
+    throw new Error('Failed to fetch job data');
+  }
 
   return (
-    <>
-      <div className={styles.page}>
-        <h1>Choose Your Class</h1>
-        {jobData.map((job, index) => (
-          <div key={index} className="">
-            <h2 className="">{job['Job Title']}</h2>
-            <p className="">{job['Company Name']}</p>
-            <p className="">{job.Location}</p>
-            <JobDetailsButton jobID={index}></JobDetailsButton>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className={styles.page}>
+      <h1>Choose Your Class</h1>
+      {jobData.map((job, index) => (
+        <div key={index} className="">
+          <h2 className="">{job['Job Title']}</h2>
+          <p className="">{job['Company Name']}</p>
+          <p className="">{job.Location}</p>
+          <JobDetailsButton jobID={index}/>
+        </div>
+      ))}
+    </div>
   );
 }
