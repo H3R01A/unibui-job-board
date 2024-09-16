5b0dd7bce14ac52b564c3cc1826e6b560d6f43a8
H3R01A <aallen0001@gmail.com>
2024-09-15 23:15:55 -0700

    update home page.tsx styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-//import styles from "./page.module.css";
 import NavLink from '@/components/nav-link';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-#0a0a0a py-8 px-4 flex flex-col gap-5 items-center">
       <h1 className="text-3xl font-bold underline">Welcome to Job Board 2.0</h1>
-      <NavLink href={'/jobs'}>
-        Click to Find Where Your Next Journey Begins
-      </NavLink>
+      <div className="border-orange-500 hover:text-orange-600 transition duration-300 ease-in-out">
+        <NavLink href={'/jobs'}>
+          Click to Find Where Your Next Journey Begins
+        </NavLink>
+      </div>
     </div>
   );
 }
