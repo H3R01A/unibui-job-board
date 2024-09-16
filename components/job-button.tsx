e9bdda8b1a68369d2e289466531d4d548f43c23d
H3R01A <aallen0001@gmail.com>
2024-09-15 23:15:28 -0700

    update button styling

--- components/job-button.tsx
+++ components/job-button.tsx
@@ -10,7 +10,7 @@ export default function JobButton() {
 
   return (
     <>
-      <div className="border-solid border-2 px-12 border-sky-500 text-center">
+      <div className="border-solid border-2 px-2 border-orange-500 rounded-md hover:bg-orange-600 text-center transition duration-300 ease-in-out">
         <button onClick={handleButtonClick}>Back to Jobs</button>
       </div>
     </>
