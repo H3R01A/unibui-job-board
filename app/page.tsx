5391a4997d2c0e9480f65c3b390b0a7ce6e4111d
H3R01A <aallen0001@gmail.com>
2024-09-20 18:08:04 -0700

    update home page with ability to see saved jobs from local storage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+'use client';
 import NavLink from '@/components/nav-link';
-
+import { useEffect, useState } from 'react';
+import Jobs from '@/components/jobs';
 export default function Home() {
+  const [favrotieJobs, setFavoriteJobs] = useState([]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const savedJobs: [] = JSON.parse(
+        localStorage.getItem('savedJobs') as string
+      );
+      setFavoriteJobs(savedJobs);
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-#0a0a0a py-8 px-4 flex flex-col gap-5 items-center">
       <h1 className="text-3xl font-bold underline">Welcome to Job Board 2.0</h1>
@@ -8,6 +21,14 @@ export default function Home() {
         <NavLink href={'/jobs'}>
           Click to Find Where Your Next Journey Begins
         </NavLink>
+      </div>
+      <h2 className="text-2xl font-bold underline">Saved Jobs</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {favrotieJobs === null ? (
+          'You have no saved jobs'
+        ) : (
+          <Jobs jobData={favrotieJobs} />
+        )}
       </div>
     </div>
   );
